bb7269d6d5da54dd6c421fea3743b936779e5369
hubiqing <hubiqing01@58ganji.com>
2017-09-07 23:23:45 +0800

    init-new

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,34 +1,31 @@
 <!-- src/components/Hello.vue -->
 <template>
     <div>
-        <div class="greeting">Hello {{name}}{{exclamationMarks}}</div>
+        <div class="greeting">Hello {{name}}</div>
         <button @click="decrement">-</button>
         <button @click="increment">+</button>
     </div>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-
+import * as  Vue from "vue" 
+const initialEnthusiasm = '11'
 export default Vue.extend({
     props: ['name', 'initialEnthusiasm'],
     data() {
         return {
-            enthusiasm: this.initialEnthusiasm,
+            enthusiasm: initialEnthusiasm
         }
     },
     methods: {
-        increment() { this.enthusiasm++; },
+        increment() {
+            console.log('increment')
+        },
         decrement() {
-            if (this.enthusiasm > 1) {
-                this.enthusiasm--;
-            }
+            console.log('decrement')
         },
     },
     computed: {
-        exclamationMarks(): string {
-            return Array(this.enthusiasm + 1).join('!');
-        }
     }
 });
 </script>
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import Vue from 'vue'
+import * as  Vue from 'vue'
 import HelloComponent from './components/Hello.vue'
 
 let v = new Vue({
