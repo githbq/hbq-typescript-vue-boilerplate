4dee3da69635db65035332d01ea8009d28a64250
hbq <hbqhbq123@qq.com>
2017-09-13 00:47:59 +0800

    files

--- dev-config/configs/rules.js
+++ dev-config/configs/rules.js
@@ -52,11 +52,29 @@ let rules = [ // 定义各种loader
       // other vue-loader options go here
     }
   },
+  //files
   {
-    test: /\.(png|jpg|gif|svg)$/,
-    loader: 'file-loader',
+    test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+    loader: 'url-loader',
     options: {
-      name: '[name].[ext]?[hash:8]'
+      limit: 10000,
+      name: 'images/[name].[hash:7].[ext]'
+    }
+  },
+  {
+    test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+    loader: 'url-loader',
+    options: {
+      limit: 10000,
+      name: 'media/[name].[hash:7].[ext]'
+    }
+  },
+  {
+    test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+    loader: 'url-loader',
+    options: {
+      limit: 10000,
+      name: 'fonts/[name].[hash:7].[ext]'
     }
   }
 ]
