00821d68f47e0be43b85b1dbf68a6c7a8124c9a4
hbq <hbqhbq123@qq.com>
2017-09-15 22:55:14 +0800

    ok

--- dev-config/configs/globalConfig.js
+++ dev-config/configs/globalConfig.js
@@ -2,10 +2,8 @@
  * 全局配置
  */
 const { templateObject } = require('./entry')
-function isWebpackDevServer() {
-  return process.argv[1] && !!(/webpack-dev-server/.exec(process.argv[1]))
-}
-const { APPS_PATH, __DEV__, NODE_ENV, pathTool } = require('./constants')
+
+const { APPS_PATH, __DEV__, NODE_ENV, pathTool, isWebpackDevServer } = require('./constants')
 const srcRelative = pathTool.relative.bind(pathTool, APPS_PATH)
 const htmlPaths = Object.keys(templateObject).map(n => srcRelative(templateObject[n]))
 
@@ -14,7 +12,7 @@ module.exports = {
   title: 'ts-vue-boilerplate',
   apiUrl: `http://localhost:3604`, //如果配了这个  就会走后端的反向代理
   favicon: '/assets/favicon.ico',
-  isWebpackDevServer: isWebpackDevServer(),
+  isWebpackDevServer,
   __DEV__,
   NODE_ENV
 }
